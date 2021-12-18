f6d9326eebf97370c95d215e02113319ef0d758e
andrey <a4412308@gmail.com>
2021-12-18 22:01:41 +0200

    reverse + namespaces, DEELETE- buttons, teachers routing, django admin updates

--- school/group/admin.py
+++ school/group/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-
+from django.contrib.admin import ModelAdmin
 # Register your models here.
+
+
 from .models import Group
 
 
-admin.site.register(Group)
+class GroupAdmin(ModelAdmin):
+    list_display = ('group_name', 'group_theme', 'teacher',)
+    list_display_links = ('group_name',)  # links from admin to edit
+    fields = ('group_name', 'group_theme', 'teacher',)
+
+    def get_queryset(self, request):
+        return Group.objects.all()
+
+
+admin.site.register(Group, GroupAdmin)
--- school/students/urls.py
+++ school/students/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', views.get_students, name='list'),
     path('create/', views.create_students, name='create'),
     path('<int:student_id>/edit/', views.edit_student, name='edit'),
+    path('<int:student_id>/delete/', views.delete_student, name='delete_student'),
     # path('<int:student_id>/', views.get_student, name='student_details'),
     # path('generate_students/', views.generate_students),
 ]
