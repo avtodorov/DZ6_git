f0b3f6261d3e977f87510eedbe7e57bb5474035e
andrey <a4412308@gmail.com>
2022-01-14 20:22:05 +0200

    lesson 10 changes

---
+++ school/students/tasks.py
@@ -0,0 +1,9 @@
+# from celery import shared_task
+# from students.models import Student
+#
+#
+# @shared_task
+# def calculate_students():
+#     count = Student.objects.count()
+#     print(f'Students count: {count}')
+#     return count
